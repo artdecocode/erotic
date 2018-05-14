9eb80bd1cf8539d4428e99a7bc91e420120526ae
Anton <anton@adc.sh>
2018-05-14 21:03:23 +0100

    build transparency

--- build/callback.js
+++ build/callback.js
@@ -1,24 +1,25 @@
-Object.defineProperty(exports, "__esModule", { value: true });exports.makeCallback = makeCallback;var _lib = require("./lib");
+Object.defineProperty(exports, "__esModule", { value: true });exports.makeCallback = makeCallback;var _cleanStack = _interopRequireDefault(require("clean-stack"));
+var _lib = require("./lib");function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
 
 
-var _cleanStack = _interopRequireDefault(require("clean-stack"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
 
 /**
-                                                                                                                                                                * Create a callback
-                                                                                                                                                                * @function makeCallback
-                                                                                                                                                                * @param {function} entryCaller function which was called at entry
-                                                                                                                                                                * @param {string} entryStack first line of the error stack to be returned
-                                                                                                                                                                * @private
-                                                                                                                                                                */
-function makeCallback(entryCaller, entryStack) {
+                                                                                                                           * Create a callback
+                                                                                                                           * @function makeCallback
+                                                                                                                           * @param {function} entryCaller function which was called at entry
+                                                                                                                           * @param {string} entryStack first line of the error stack to be returned
+                                                                                                                           * @param {boolean} [shadow=false] print only entry stack
+                                                                                                                           * @private
+                                                                                                                           */
+function makeCallback(entryCaller, entryStack, shadow = false) {
   /**
-                                                 * This callback should be called when an asynchronous error occured.
-                                                 * @param {(string|Error)} messageOrError A message string or an Error object at
-                                                 * the point of actual error.
-                                                 * @returns {Error} An error with an updated stack which should be throw, or
-                                                 * rejected with.
-                                                 */
-  function eroticCallback(messageOrError) {
+                                                                 * This callback should be called when an asynchronous error occurred.
+                                                                 * @param {(string|Error)} messageOrError A message string or an Error object at
+                                                                 * the point of actual error.
+                                                                 * @returns {Error} An error with an updated stack which should be throw, or
+                                                                 * rejected with.
+                                                                 */
+  function cb(messageOrError) {
     const caller = (0, _lib.getCallerFromArguments)(arguments);
     const { stack: errorStack } = new Error();
     const calleeStackLine = (0, _lib.getCalleeStackLine)(errorStack);
@@ -27,21 +28,19 @@ function makeCallback(entryCaller, entryStack) {
 
     const stackHeading = (0, _lib.getStackHeading)(message);
     const entryHasCallee = entryCaller === caller;
-    const stackMessage = [stackHeading].
-    concat(entryHasCallee ?
-    entryStack :
-    [
+    const stackMessage = [
+    stackHeading,
+    ...(entryHasCallee || shadow ? [entryStack] : [
     calleeStackLine,
-    entryStack]).
+    entryStack])].
 
     join('\n');
+
     const stack = (0, _cleanStack.default)(stackMessage);
     const properties = { message, stack };
-    return Object.assign(new Error(), isError ?
-    Object.assign({}, messageOrError, properties) :
-    properties);
 
+    return Object.assign(isError ? messageOrError : new Error(), properties);
   }
 
-  return eroticCallback;
+  return cb;
 }
--- build/index.js
+++ build/index.js
@@ -6,11 +6,12 @@ var _callback = require("./callback");
 /**
                                         * Call this method to get a function that will return an error with a stack
                                         * trace starting at the line in code when the call was made.
+                                        * @param {boolean} [transparent] Pretend as if the call to the function lead to the error, without exposing any of the internal stack.
                                         */
-function erotic() {
-  const error = new Error();
+function erotic(transparent) {
+  const { stack } = new Error();
   const caller = (0, _lib.getCallerFromArguments)(arguments);
-  const entryStack = (0, _lib.getEntryStack)(error.stack);
+  const entryStack = (0, _lib.getEntryStack)(stack, transparent);
 
-  return (0, _callback.makeCallback)(caller, entryStack);
+  return (0, _callback.makeCallback)(caller, entryStack, transparent);
 }
